3da9495dbb78447c5c8dadc7a738666075a9f85e
Yeonjung Choi <40679419+YeonjungChoi97@users.noreply.github.com>
2022-05-14 19:41:52 +0900

    Create DFSandBFS.py

---
+++ graph theory/DFSandBFS.py
@@ -0,0 +1,3 @@
+#백준 1260
+#DFS: Depth First Search, uses stack to find the shortest path, better when target is far from source, faster
+#BFS: Breadth First Search, uses Queue to find the shortest path, better when target is closer to source, slower
