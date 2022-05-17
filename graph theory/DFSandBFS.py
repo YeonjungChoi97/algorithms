000b50cb8eabf7b26c8e1de273f35ed8d68fa60a
Yeonjung Choi <40679419+YeonjungChoi97@users.noreply.github.com>
2022-05-17 15:59:50 +0900

    Update DFSandBFS.py

--- graph theory/DFSandBFS.py
+++ graph theory/DFSandBFS.py
@@ -4,39 +4,43 @@
 
 from collections import deque
 
-#첫째줄에 n,m,v 들어옴
+# 첫째줄에 n,m,v 들어옴
 n, m, v = map(int, input().split())
 
-#graph 를 initialise 함, graph[0]의 포지션은 비워두기위해 N+1 의 길이를 갖는 리스트 생성 
+# graph 를 initialise 함, graph[0]의 포지션은 비워두기위해 N+1 의 길이를 갖는 리스트 생성
 graph = [[] for _ in range(n + 1)]
 
 for _ in range(m):
-  m1, m2 = map(int, input().split())
-  graph[m1].append(m2)
-  graph[m2].append(m1)
-  
+    m1, m2 = map(int, input().split())
+    graph[m1].append(m2)
+    graph[m2].append(m1)
+
+
 def dfs(graph, v, visited):
     visited[v] = True
-    print(v, end= ' ')
+    print(v, end=' ')
     for i in graph[v]:
-        if not visited[1]:
-            ds(graph, i, visited)
+        if not visited[i]:
+            dfs(graph, i, visited)
+
 
 def bfs(graph, start, visited):
     queue = deque([start])
     visited[start] = True
     while queue:
         v = queue.popleft()
-        print(v, end= ' ')
+        print(v, end=' ')
         for i in graph[v]:
-            if not visited[1]:
+            if not visited[i]:
                 queue.append(i)
                 visited[i] = True
-                
-visited = [False] * n
+
+
+visited = [False] * (n+1)
 
 dfs(graph, v, visited)
+print("")
 
-visited = [False] * n
+visited = [False] * (n+1)
 
 bfs(graph, v, visited)
